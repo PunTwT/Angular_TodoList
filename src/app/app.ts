664d68f5be1b5abf558848ba6795049b0ce448e9
PunTwT <punnathornm@gmail.com>
2026-02-18 23:50:42 +0700

    Angular Todo List

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('an-todo');
+  newTask: string='';
+  newStatus: string='todo';
+  tasks: any[] = [];
+
+  addTask() {
+    if (this.newTask.trim() !== '') {
+      this.tasks.push({
+        taskname : this.newTask,
+        status: this.newStatus
+      });
+      this.newTask = '';
+      this.newStatus = 'todo';
+    }
+  }
+
+  removeTask(index: number) {
+    this.tasks.splice(index, 1);
+  }
+
+  toggleTask(index: number) {
+    this.tasks[index].completed = !this.tasks[index].completed;
+  }
 }
